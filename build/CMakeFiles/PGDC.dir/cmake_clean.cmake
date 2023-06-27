cca6b795f176d239ab40668e065d27f5e8a3a1b3
Shayan <skhos068@uottawa.ca>
2023-06-27 17:11:01 -0400

    updated

---
+++ build/CMakeFiles/PGDC.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PGDC.dir/main.cpp.o"
+  "CMakeFiles/PGDC.dir/main.cpp.o.d"
+  "PGDC"
+  "PGDC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PGDC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
